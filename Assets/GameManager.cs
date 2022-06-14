d4fb175ae59cf45bbbf2b32b90648bee9bebd313
AceofGrades <47581354+LorandSol@users.noreply.github.com>
2022-06-14 11:53:55 +1000

    Added second spawn point for platform
    will need reviewing later

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject plaformPrefab;
+    public GameObject plaformPrefab1;
+    public GameObject plaformPrefab2;
 
-    public int platformCount = 300;
+    public int platform1Count = 150;
+    public int platform2Count = 150;
 
     private void Start()
     {
-        Vector3 spawnPosition = new Vector3();
+        Vector3 spawnPosition1 = new Vector3();
+        //Vector3 spawnPosition2 = new Vector3();
 
-        for (int i = 0; i < platformCount; i++)
+        for (int i = 0; i < platform1Count; i++)
         {
-            spawnPosition.y += Random.Range(2f, 5f);
-            spawnPosition.x = Random.Range(-3f, 3f);
-            Instantiate(plaformPrefab, spawnPosition, Quaternion.identity);
+            spawnPosition1.y += Random.Range(2f, 5f);
+            spawnPosition1.x = Random.Range(-3f, 3f);
+            Instantiate(plaformPrefab1, spawnPosition1, Quaternion.identity);
         }
+
+        //for (int i = 0; i < platform2Count; i++)
+        //{
+        //    spawnPosition2.y += Random.Range(5f, 8f);
+        //    spawnPosition2.x = Random.Range(-3f, 3f);
+        //    Instantiate(plaformPrefab2, spawnPosition2, Quaternion.identity);
+        //}
     }
 }
